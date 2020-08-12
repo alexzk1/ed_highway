242255c74096a0fead4de4d496b7afc0ff256e24
alexzk1 <alexzkhr@gmail.com>
2020-08-13 01:24:47 +0300

    added jump mode to fuel calc

--- widgets/calcstab.cpp
+++ widgets/calcstab.cpp
@@ -6,6 +6,8 @@
 #include "config_ui/globalsettings.h"
 
 constexpr static int max_carrier_cargo = 25000;
+constexpr static int carrier_tank_size = 1000;
+
 const static QString settingsGroup = "CalcsTabSettings";
 
 CalcsTab::CalcsTab(QWidget *parent) :
@@ -13,6 +15,7 @@ CalcsTab::CalcsTab(QWidget *parent) :
     ui(new Ui::CalcsTab),
     delayedStart(new DelayedSignal(this))
 {
+    constexpr static int delay_ms = 2000;
     ui->setupUi(this);
     connect(delayedStart, &DelayedSignal::delayedSignal, this, &CalcsTab::calcCarrierFuel);
 
@@ -21,7 +24,15 @@ CalcsTab::CalcsTab(QWidget *parent) :
         ptr->setMaximum(max_carrier_cargo);
         connect(ptr, QOverload<int>::of(&QSpinBox::valueChanged), this, [this](int)
         {
-            delayedStart->sourceSignal(2000);
+            delayedStart->sourceSignal(delay_ms);
+        });
+    };
+
+    const auto setup_radio = [this](QRadioButton * rb)
+    {
+        connect(rb, &QRadioButton::toggled, this, [this](bool)
+        {
+            delayedStart->sourceSignal(delay_ms / 4);
         });
     };
 
@@ -29,6 +40,8 @@ CalcsTab::CalcsTab(QWidget *parent) :
     setup_spin(ui->sbModules);
     setup_spin(ui->sbFuel);
 
+    setup_radio(ui->rbOnEmpty);
+    setup_radio(ui->rbTankFull);
 
     loadSettings();
 }
@@ -88,7 +101,13 @@ void CalcsTab::calcCarrierFuel()
     const auto mods = ui->sbModules->value();
     const auto carg = ui->sbCargo->value();
     const auto fuel = ui->sbFuel->value();
-    ui->lblMass->setText(tr("Non-fuel mass of carrier: %1(t). This should be same as total mass - tritium mass.").arg(mods + carg));
+
+    const bool keep_full    = ui->rbTankFull->isChecked();
+    const bool refuel_empty = ui->rbOnEmpty->isChecked();
+
+    setTritiumStepping();
+
+    ui->lblMass->setText(tr("Non-fuel mass of carrier: %1(t). This should be same as (total mass - tritium mass).").arg(mods + carg));
 
     if (mods + carg + fuel > max_carrier_cargo)
         ui->lblResult->setText(tr("Total mass is bigger then maximum cargo %1(t).").arg(max_carrier_cargo));
@@ -99,16 +118,67 @@ void CalcsTab::calcCarrierFuel()
         constexpr static float jd_mul = jump_distance / 4.f;
 
         int distance = 0;
-        for (int current_fuel = fuel, current_used = 0; current_fuel > current_used; current_fuel -= current_used)
+
+        const auto jump = [&distance]()
         {
-            //=ROUND(10 + 500 * (1 + (D2 + $C$2)/25000) / 4)
-            current_used = round(10 + jd_mul * (1 + (current_fuel + carg + mods) / max_cargo));
             distance += jump_distance;
+        };
+
+        for (int current_fuel = fuel, current_used = 0, tank = carrier_tank_size; current_fuel + tank > current_used;)
+        {
+            for (int r = 0; r < 2; ++r)
+            {
+                const int total = current_fuel + tank;
+                if (total < carrier_tank_size)
+                {
+                    current_fuel = 0;
+                    tank = total;
+                }
+                current_used = round(10 + jd_mul * (1 + (current_fuel + carg + mods) / max_cargo));
+
+                if (keep_full)
+                {
+
+                    if (current_fuel > current_used)
+                    {
+                        current_fuel -= current_used;
+                        jump();
+                    }
+                    else
+                    {
+
+                        if (tank > current_used)
+                        {
+                            tank -= current_used;
+                            jump();
+                        }
+                    }
+                    break;
+                }
+
+                if (refuel_empty)
+                {
+                    if (current_used > total)
+                        break;
+
+                    if (tank < current_used)
+                    {
+                        const int delta = std::min(carrier_tank_size - tank, current_fuel);
+                        tank += delta;
+                        current_fuel -= delta;
+                    }
+                    else
+                    {
+                        tank -= current_used;
+                        jump();
+                        break;
+                    }
+                }
+            }
         }
 
         ui->lblResult->setText(tr("Max distance: %1 (ly). With return same way: %2 (ly).").arg(distance).arg(distance / 2));
     }
-    setTritiumStepping();
 }
 
 void CalcsTab::setTritiumStepping()
